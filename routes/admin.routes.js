dd81a77bf46e8a9029fff7fd1e6fd27060c732bb
Sachin <sachin.cs23@bitsathy.ac.in>
2026-03-18 15:04:20 +0530

    added backend for warning system

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { verifyReport, getStudents } = require('../controllers/admin.controller');
+const { verifyReport, getStudents, getWarnings } = require('../controllers/admin.controller');
 const { protect } = require('../middleware/auth.middleware');
 const { allowRoles } = require('../middleware/role.middleware');
 
 router.put('/verify/:reportId', protect, allowRoles('admin'), verifyReport);
 router.get('/users',            protect, allowRoles('admin'), getStudents);
+router.get('/warnings',         protect, allowRoles('admin'), getWarnings);
 
 module.exports = router;
